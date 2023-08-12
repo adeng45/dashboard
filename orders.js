d424d737f1399c087fc82202464b03ce8cb62570
Anthony <andeng2@berkeley.edu>
2023-08-12 00:00:04 -0700

    i can't feel my legs

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'CS61A',
+        productNumber: '1',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CS61B',
+        productNumber: '2',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'CS170',
+        productNumber: '3',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
